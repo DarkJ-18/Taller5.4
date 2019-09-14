ab7eee1509447e5abf006c459532e0b9e503c913
Juan <j.juancamilojurado@gmail.com>
2019-09-13 23:29:51 -0500

    Initial commit

---
+++ Taller5.4/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace Taller5._4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btniniciar = new System.Windows.Forms.Button();
+            this.btnparar = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lbltiempo = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtsegundos = new System.Windows.Forms.TextBox();
+            this.txthoras = new System.Windows.Forms.TextBox();
+            this.txtminutos = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btniniciar
+            // 
+            this.btniniciar.Location = new System.Drawing.Point(61, 142);
+            this.btniniciar.Name = "btniniciar";
+            this.btniniciar.Size = new System.Drawing.Size(75, 23);
+            this.btniniciar.TabIndex = 1;
+            this.btniniciar.Text = "Iniciar";
+            this.btniniciar.UseVisualStyleBackColor = true;
+            this.btniniciar.Click += new System.EventHandler(this.Btniniciar_Click);
+            // 
+            // btnparar
+            // 
+            this.btnparar.Location = new System.Drawing.Point(203, 142);
+            this.btnparar.Name = "btnparar";
+            this.btnparar.Size = new System.Drawing.Size(75, 23);
+            this.btnparar.TabIndex = 2;
+            this.btnparar.Text = "Parar";
+            this.btnparar.UseVisualStyleBackColor = true;
+            this.btnparar.Click += new System.EventHandler(this.Btnparar_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // lbltiempo
+            // 
+            this.lbltiempo.AutoSize = true;
+            this.lbltiempo.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltiempo.Location = new System.Drawing.Point(98, 88);
+            this.lbltiempo.Name = "lbltiempo";
+            this.lbltiempo.Size = new System.Drawing.Size(125, 32);
+            this.lbltiempo.TabIndex = 3;
+            this.lbltiempo.Text = "00:00:00";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(22, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 19);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Horas:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(127, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 19);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Minutos:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(228, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 19);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Segundos:";
+            // 
+            // txtsegundos
+            // 
+            this.txtsegundos.Location = new System.Drawing.Point(232, 43);
+            this.txtsegundos.Name = "txtsegundos";
+            this.txtsegundos.Size = new System.Drawing.Size(57, 20);
+            this.txtsegundos.TabIndex = 7;
+            // 
+            // txthoras
+            // 
+            this.txthoras.Location = new System.Drawing.Point(26, 43);
+            this.txthoras.Name = "txthoras";
+            this.txthoras.Size = new System.Drawing.Size(57, 20);
+            this.txthoras.TabIndex = 8;
+            // 
+            // txtminutos
+            // 
+            this.txtminutos.Location = new System.Drawing.Point(131, 43);
+            this.txtminutos.Name = "txtminutos";
+            this.txtminutos.Size = new System.Drawing.Size(57, 20);
+            this.txtminutos.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 187);
+            this.Controls.Add(this.txtminutos);
+            this.Controls.Add(this.txthoras);
+            this.Controls.Add(this.txtsegundos);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbltiempo);
+            this.Controls.Add(this.btnparar);
+            this.Controls.Add(this.btniniciar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btniniciar;
+        private System.Windows.Forms.Button btnparar;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lbltiempo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtsegundos;
+        private System.Windows.Forms.TextBox txthoras;
+        private System.Windows.Forms.TextBox txtminutos;
+    }
+}
+
